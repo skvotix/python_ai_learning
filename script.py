7d85772475827ffc7bc9129f7443589f005dffd2
ughsxl <101698689+ughsxl@users.noreply.github.com>
2025-12-09 16:46:11 +0100

    Class decorator

--- script.py
+++ script.py
@@ -1,62 +1,22 @@
-class DataDescriptor:
-    def __init__(self, expected_type):
-        self.expected_type = expected_type
+class ClassPresentator:
+    def __init__(self, cls):
+        self.__cls = cls
+
+    def __call__(self, *args, **kwargs):
+        print(f"Created object of class {self.__cls.__name__} with args {args}, kwargs {kwargs}")
+        return self.__cls(*args, **kwargs)
 
 
-    def __set_name__(self, owner, name):
-        self.name = "_" + name
+@ClassPresentator
+class Car:
+    def __init__(self, model, year):
+        self.model = model
+        self.year = year
 
-    def __get__(self, instance, owner):
-        return getattr(instance, self.name)
-
-    def __set__(self, instance, value):
-        if not isinstance(value, self.expected_type):
-            raise TypeError(f"Expected {self.expected_type}, got {type(value)}")
-
-        setattr(instance, self.name, value)
+    def __call__(self):
+        print("Hey!")
 
 
 
-class NonDataDescriptor:
-    def __init__(self, default_value):
-        self.default_value = default_value
-
-    def __set_name__(self, owner, name):
-        self.name = "_" + name
-
-    def __get__(self, instance, owner):
-        return self.default_value
-
-class User:
-    name = DataDescriptor(str)
-    age = DataDescriptor(int)
-    role = NonDataDescriptor("user")
-    theme = NonDataDescriptor("light")
-
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-
-    def give_a_role(self, role):
-        self.role = role
-
-    def switch_theme(self):
-        if self.theme == "light":
-            self.theme = "dark"
-        elif self.theme == "dark":
-            self.theme = "light"
-        else:
-            self.theme = "light"
-
-
-    def __str__(self):
-        return f"{self.name} {self.age} {self.role} {self.theme}"
-
-
-user = User("Illia", 17)
-print(user)
-user.give_a_role("king")
-print(user.role)
-user.switch_theme()
-print(user)
+toyota = Car("Toyota", 1999)
+toyota()
