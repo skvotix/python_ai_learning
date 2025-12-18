73c3aad6dce590fe2721d464de5904235fb85305
ughsxl <101698689+ughsxl@users.noreply.github.com>
2025-12-18 20:41:43 +0100

    Inheritance

--- script.py
+++ script.py
@@ -1,33 +1,30 @@
-class FRange:
-    def __init__(self, start = 0.0, stop = 0.0, step = 1.0):
-        self.start = start
-        self.stop = stop
-        self.step = step
-        self.__value = self.start - self.step
+class Geometry:
+    class_name = "Geometry"
 
 
-    def __iter__(self):
-        self.__value = self.start - self.step
-        return self
+    def set_coords(self, *args):
+        self.coords = args
 
-    def __next__(self):
-        if self.__value + self.step < self.stop:
-            self.__value += self.step
-            return self.__value
-        else:
-            raise StopIteration
+    def get_coords(self):
+        return tuple(self.coords)
 
-fr = FRange(0, 2, 0.5)
 
-it = iter(fr)
-print(next(it))
-print(next(it))
-print(next(it))
-print(next(it))
+    def __call__(self, *args, **kwargs):
+        self.set_coords(*args)
 
-it = iter(fr)
 
-print(next(it))
-print(next(it))
-print(next(it))
-print(next(it))
+class Line(Geometry):
+
+    def draw(self):
+        print("Drawing a line")
+
+
+
+class Rectangle(Geometry):
+
+    def draw(self):
+        print("Рисование прямоугольника")
+
+
+
+
