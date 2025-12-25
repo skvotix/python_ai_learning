fcca1ebbbcce310a71d5240f38543a87f92bb86f
ughsxl <101698689+ughsxl@users.noreply.github.com>
2025-12-25 14:28:36 +0100

    super() and overloading

--- script.py
+++ script.py
@@ -1,30 +1,29 @@
 class Geometry:
-    class_name = "Geometry"
+    name = "Geometry"
 
+    def __init__(self, x1, y1, x2, y2):
+        print(f"Initializator Geometry for {self.__class__} class")
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
 
-    def set_coords(self, *args):
-        self.coords = args
-
-    def get_coords(self):
-        return tuple(self.coords)
-
-
-    def __call__(self, *args, **kwargs):
-        self.set_coords(*args)
 
 
 class Line(Geometry):
-
     def draw(self):
         print("Drawing a line")
 
-
-
 class Rectangle(Geometry):
+    def __init__(self, x1, y1, x2, y2, fill = None):
+        super().__init__(x1, y1, x2, y2)
+        print("Initializator Rectangle")
+        self.fill = fill
 
     def draw(self):
-        print("Рисование прямоугольника")
+        print("Drawing a rectangle")
 
 
 
-
+r = Rectangle(2, 3, 4, 5, "Blue")
+l = Line(1, 2, 3, 4)
